004242c94e95281617fae70a44f82bdf8e7333f7
@mariacarolinaboabaid <carolkboabaid@gmail.com>
2023-04-18 20:09:43 -0300

    Adicionando o projeto pronto para o repositório.
    Adicionando o projeto pronto para o repositório.

---
+++ projeto/labSchool/labSchool/Models/Pedagogo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace labSchool.Models
+{
+    // CLASSE PEDAGOGO COM HERANÇA DA CLASSE PESSOA
+    public class Pedagogo : Pessoa
+	{
+        public int TotalAtendimentosPedagogicos { get; set; } 
+
+        // MET PARA INCREMENTER O NÚMERO DE ATENDIMENTOS
+        public void AdicionarAtendimento()
+        {
+            TotalAtendimentosPedagogicos += 1;
+        }
+    }
+}
+
